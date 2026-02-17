72693977cc94dea8ee347f4bddc2eb684cabdd5d
Renisha537 <rr0020@srmist.edu.in>
2026-02-17 15:12:14 +0530

    Renisha537 Added UC2

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -11,5 +11,24 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Management System \nVersion : 1.0 \nSystem initialized successfully");
+        String input="madam";
+
+        /**This class demonstrates basic palindrome validation using a hardcoded string value
+         * at this stage the application;
+         * stores a predefined string and checks if it is palindrome and then displays the result
+         * UC2 introduces fundamental comparison logic before using advanced data structures.
+         * @author Renisha
+         * @version 1.1
+         */
+        System.out.println("Input text: madam");
+        System.out.print("Is it a palindrome?:");
+        for(int i=0;i<input.length()/2;i++){
+            if(input.charAt(i)!=input.charAt(input.length()-i-1)) {
+                System.out.print(" false");
+            }else{
+                System.out.print(" True");
+                break;
+            }
+        }
     }
 }
